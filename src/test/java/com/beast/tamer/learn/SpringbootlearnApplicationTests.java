70c3faaa8aa1ed8b240ede3cbc8441883fc93990
jim <33473633+wypzj@users.noreply.github.com>
2020-01-19 14:19:50 +0800

    Initial commit

---
+++ src/test/java/com/beast/tamer/learn/SpringbootlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beast.tamer.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
